03d5d80cb0972db02215a8473a12bc3830f36d88
Yaroslav Benin <dondublon1@yandex.ru>
2023-05-11 16:22:06 +0400

    Using author details ans context.

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -8,29 +8,47 @@ user_model = get_user_model()
 
 register = template.Library()
 
-@register.filter
-def author_details(author: user_model, current_user=None):
-  if not isinstance(author, user_model):
-    # return empty string as safe default
-    return ""
 
-  if author == current_user:
-    return format_html("<strong>me</strong>")
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
-  if author.first_name and author.last_name:
-    name = f'{author.first_name} {author.last_name}'
-  else:
-    name = author.username
-  if author.email:
-    name = format_html(f'<a href="mailto:{author.email}">{name}</a>')
-  return format_html(name)
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
 
 
-@register.simple_tag
-def row():
-    return format_html('<div class="row">')
+@register.simple_tag  # take_context=True to get the context
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
 
 
 @register.simple_tag
 def endrow():
+    return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
     return format_html("</div>")
